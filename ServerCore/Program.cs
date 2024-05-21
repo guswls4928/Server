50b05c0ac47ba99d6abb45e93f00e2b2a23740e8
현진혁 <guswls4928@naver.com>
2024-05-21 12:26:03 +0900

    initial commit

---
+++ ServerCore/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ServerCore
+{
+    class Program
+    {
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("Thread is started");
+            while (!_stop)
+            {
+
+            }
+            Console.WriteLine("Thread is stopped");
+        }
+
+        static void Main(string[] args)
+        {
+            Task t = new Task(ThreadMain);
+            t.Start();
+
+            Thread.Sleep(1000);
+
+            _stop = true;
+
+            Console.WriteLine("stop 호출");
+            Console.WriteLine("종료 대기중");
+
+            t.Wait();
+
+            Console.WriteLine("종료 완료");
+        }
+    }
+}
