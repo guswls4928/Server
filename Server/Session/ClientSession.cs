0dda7ea3b360bb7e249b3fbd821eb459da565a45
현진혁 <guswls4928@naver.com>
2024-07-30 21:16:07 +0900

    Unity Link #3

--- Server/Session/ClientSession.cs
+++ Server/Session/ClientSession.cs
@@ -13,8 +13,11 @@ namespace Server
 	{
 		public int SessionId { get; set; }
 		public GameRoom Room { get; set; }
+		public float PosX { get; set; }
+        public float PosY { get; set; }
+        public float PosZ { get; set; }
 
-		public override void OnConnected(EndPoint endPoint)
+        public override void OnConnected(EndPoint endPoint)
 		{
 			Console.WriteLine($"OnConnected : {endPoint}");
 
